d20674316d405d2ef72d4731aff6d42415ed2551
Stan Brinkerhoff <stan.brinkerhoff@coxautoinc.com>
2023-01-03 16:44:18 -0500

    chore: black

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,15 +4,13 @@ import datetime
 
 
 def test_try_parse_data():
-    data = {
-        "date": "2022-01-01T00:00:00Z",
-        "consumed": 10.5
-    }
+    data = {"date": "2022-01-01T00:00:00Z", "consumed": 10.5}
     result = list(Usage.try_parse_data(data))
     assert len(result) == 1
     assert isinstance(result[0], Usage)
     assert result[0].start_time == datetime.datetime(2022, 1, 1, 0, 0, 0)
     assert result[0].consumed_kwh == 10.5
+
 
 @patch("greenmountainpower.api.requests_oauthlib.OAuth2Session")
 def test_instantiate_client(mock_oauth2_session):
